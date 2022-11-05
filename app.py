b71ce7c1a8c3e9a31028ac656c1b58d0906ecf9a
Afrizal Meka Mulyana <afrizalmeka@gmail.com>
2022-11-05 16:27:57 +0700

    version beta

--- app.py
+++ app.py
@@ -1,43 +1,69 @@
 from flask import Flask, request, render_template
-import pickle
+import pandas as pd
 
 app = Flask(__name__)
 
-model_file = open('model.pkl', 'rb')
-model = pickle.load(model_file, encoding='bytes')
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', hasil_prediksi_tanaman="")
+    return render_template('index.html', nilai_ds=0, nilai_nlp=0, nilai_cv=0, nilai_tech=0, hasil="")
 
 @app.route('/predict', methods=['POST'])
 def predict():
     '''
-    Predict the insurance cost based on user inputs
-    and render the result to the html page
+    Memvalidasi data siswa, jika sesuai akan menampilkan nilainya.
     '''
     nama, nim, univ, prodi, nomor_hp  = [x for x in request.form.values()]
 
-    data = [nama, nim, univ, prodi, nomor_hp]
+    data = []
 
-    data.append(float(nitrogen))
-    data.append(float(fosfor))
-    data.append(float(kalium))
-    data.append(float(suhu))
-    data.append(float(kelembaban))
-    data.append(float(phtanah))
-    data.append(float(curahhujan))
+    data.append(str(nama).lower())
+    data.append(str(nim).lower())
+    data.append(str(univ).lower())
+    data.append(str(prodi).lower())
+    data.append(int(nomor_hp))
+
+    dir = "database/database.xlsx"
+    df = pd.read_excel(dir)
     
-    #inisialisasi list nama tanaman, dikarenakan model di standarisasi
-    nama = {0 : 'Tanaman Padi', 1 : 'Tanaman Jagung', 2 : 'Tanaman Kacang Chickpea', 3 : 'Tanaman Kacang Merah', 4 : 'Tanaman Kacang Polong',
-        5 : 'Tanaman Ngengat', 6 : 'Tanaman Kacang Hijau', 7 : 'Tanaman Black Gram', 8 : 'Tanaman Lentil', 9 : 'Tanaman Buah Delima',
-        10 : 'Tanaman Buah Pisang', 11 : 'Tanaman Buah Mangga', 12 : 'Tanaman Buah Anggur', 13 : 'Tanaman Buah Semangka', 14 : 'Tanaman Buan Melon', 15 : 'Tanaman Buah Apel',
-        16 : 'Tanaman Buah Jeruk', 17 : 'Tanaman Buah Pepaya', 18 : 'Tanaman Kelapa', 19 : 'Tanaman Kapas', 20 : 'Tanaman Jute', 21 : 'Tanaman Kopi'}
+    df['nim'] = str(df['nim'])
+    # df['ds'] = int(df['ds'])
+    # ds['nlp'] = int(ds['nlp'])
+    # ds['cv'] = int(ds['cv'])
+    # ds['tech'] = int(ds['tech'])
 
-    prediction = model.predict([data])
-    output = nama[prediction[0]]
+    df['nama'] = df['nama'].str.lower()
+    df['nim'] = df['nim'].str.lower()
+    df['univ'] = df['univ'].str.lower()
+    df['prodi'] = df['prodi'].str.lower()
 
-    return render_template('index.html', hasil_prediksi_tanaman=output, nitrogen=nitrogen, fosfor=fosfor, kalium=kalium, suhu=suhu, kelembaban=kelembaban, phtanah=phtanah, curahhujan=curahhujan)
+    size = df.shape
+    size = size[0] - 1
+
+    ds = 0
+    nlp = 0
+    cv = 0
+    tech = 0
+    hasil = ''
+
+    for x in df.index:
+        name = df['nama'][x]
+        
+        if data[0] == name:
+            ds = (df['ds'][x])
+            nlp = (df['nlp'][x])
+            cv = (df['cv'][x])
+            tech = (df['tech'][x])
+
+            hasil = 'Selamat, Data ditemukan !'
+            break
+        
+        elif x == size:
+            hasil = 'Data tidak ditemukan !'
+    
+
+    return render_template('index.html', nilai_ds=ds, nilai_nlp=nlp, nilai_cv=cv, nilai_tech=tech, nama=nama, nim=nim, univ=univ, prodi=prodi, nomor_hp=nomor_hp, hasil=hasil)
 
 
 if __name__ == '__main__':
--- process.py
+++ process.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
-df['nama'] = df['nama'].str.lower()
-df['univ'] = df['univ'].str.lower()
-df['prodi'] = df['prodi'].str.lower()
+def pandas_jalankan():
+    global ds, nlp, cv, tech, df, size
+
+    dir = "/database/database.xlsx"
+    df = pd.read_excel(dir)
+
+    df['nama'] = df['nama'].str.lower()
+    df['nim'] = df['nim'].str.lower()
+    df['univ'] = df['univ'].str.lower()
+    df['prodi'] = df['prodi'].str.lower()
+
+    size = df.shape
+    size = size[0]
+
+    ds = 
