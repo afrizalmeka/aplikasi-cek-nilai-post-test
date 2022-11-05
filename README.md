9cb6752b8ed38efe888cf276b652ee3c25165e09
Afrizal Meka Mulyana <afrizalmeka@gmail.com>
2022-11-05 14:49:46 +0700

    tahap 1

---
+++ README.md
@@ -0,0 +1,1 @@
+# aplikasi-cek-nilai-post-test
---
+++ process.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df['nama'] = df['nama'].str.lower()
+df['univ'] = df['univ'].str.lower()
+df['prodi'] = df['prodi'].str.lower()
